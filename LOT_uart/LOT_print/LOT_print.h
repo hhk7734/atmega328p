c42baeb1d68caa8d4e5431c65e00bf977d0f6a0d
Hyeonki Hong <hhk7734@gmail.com>
2018-12-19 00:04:57 +0900

    전처리 제거

--- LOT_uart/LOT_print/LOT_print.h
+++ LOT_uart/LOT_print/LOT_print.h
@@ -7,7 +7,8 @@
 #ifndef _LOT_PRINT_H_
 #define _LOT_PRINT_H_
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 #include <inttypes.h>
@@ -19,40 +20,21 @@ extern "C" {
  */
 #include "LOT_uart0.h"
 /// 문자를 송신하는 매크로 함수 설정
-#define LOT_print_char( x ) LOT_uart0_write( x )
+#define LOT_print_char(x) LOT_uart0_write(x)
 /// 문자열을 송신하는 매키로 함수 설정
-#define LOT_print_str( x ) LOT_uart0_print_str( x )
-
-#define _LOT_PRINT_NL_CR_
-#define _LOT_PRINT_8_BIT_
-#define _LOT_PRINT_16_BIT_
-#define _LOT_PRINT_32_BIT_
-#define _LOT_PRINT_64_BIT_
-#define _LOT_PRINT_FLOAT_
+#define LOT_print_str(x) LOT_uart0_print_str(x)
 /// @}
 
-#ifdef _LOT_PRINT_NL_CR_
-void LOT_print_newline( void );
-#endif // _LOT_PRINT_NL_CR_
-#ifdef _LOT_PRINT_8_BIT_
-void LOT_print_u8( uint8_t data );
-void LOT_print_i8( int8_t data );
-#endif // _LOT_PRINT_8_BIT_
-#ifdef _LOT_PRINT_16_BIT_
-void LOT_print_u16( uint16_t data );
-void LOT_print_i16( int16_t data );
-#endif // _LOT_PRINT_16_BIT_
-#ifdef _LOT_PRINT_32_BIT_
-void LOT_print_u32( uint32_t data );
-void LOT_print_i32( int32_t data );
-#endif // _LOT_PRINT_32_BIT_
-#ifdef _LOT_PRINT_64_BIT_
-void LOT_print_u64( uint64_t data );
-void LOT_print_i64( int64_t data );
-#endif // _LOT_PRINT_64_BIT_
-#ifdef _LOT_PRINT_FLOAT_
-void LOT_print_f( float data, uint8_t digits );
-#endif // _LOT_PRINT_FLOAT_
+void LOT_print_newline(void);
+void LOT_print_u8(uint8_t data);
+void LOT_print_i8(int8_t data);
+void LOT_print_u16(uint16_t data);
+void LOT_print_i16(int16_t data);
+void LOT_print_u32(uint32_t data);
+void LOT_print_i32(int32_t data);
+void LOT_print_u64(uint64_t data);
+void LOT_print_i64(int64_t data);
+void LOT_print_f(float data, uint8_t digits);
 
 #ifdef __cplusplus
 }
