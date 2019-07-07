8bbd61efc183403a171db0edf149345c2e70a41c
Yeon-soo <joent939@naver.com>
2019-07-07 14:02:15 +0900

    blink PINB5

--- uart1.c
+++ uart1.c
@@ -1,5 +1,21 @@
+#define F_CPU 16000000UL
+
 #include <avr/io.h>
+#include <util/delay.h>
+
 
 int main(void)
 {
+	DDRB |= _BV(DDB5);
+
+	while(1)
+	{
+		PORTB |= _BV(PB5);
+		delay_ms(100);
+		PORTB &= ~_BV(PB5);
+		delay_ms(100);
+	}
+
+	return 0;
+
 }
